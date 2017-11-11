94108d51816dbc2f1846d82dce8ef2eaa0e94b9a
Seunghun Lee <waydi1@gmail.com>
2017-11-11 14:11:44 +0900

    Change open to io.open

--- dotenv/ipython.py
+++ dotenv/ipython.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, unicode_literals
+from io import open
 from .main import load_dotenv, find_dotenv
 
 from IPython.core.magic import Magics, magics_class, line_magic
